5db10fe1085d1a838bbc0cec7e7f6f5db40f9631
unknown <heewonp122@gmail.com>
2022-07-07 07:34:04 +0900

    [BOJ]최대힙/실버2/15분

--- heewonp/[BOJ]최대힙/[BOJ]최대힙.py
+++ heewonp/[BOJ]최대힙/[BOJ]최대힙.py
@@ -9,10 +9,8 @@ for _ in range(n):
     x = int(input())
     if x == 0:
         if heap:
-            # print(0)
             print(-heapq.heappop(heap))
         else:
-            # print(-heapq.heappop(heap))
             print(0)
     else:
         heapq.heappush(heap,-x)

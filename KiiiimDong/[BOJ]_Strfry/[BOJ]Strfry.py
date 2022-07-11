df3ec0dba3918245b945c65b543c2f7a24daef34
KiiiimDong <qazz234@naver.com>
2022-07-11 19:47:42 +0900

    [BOJ] Strfry / Bronze2 / 30분
    https://www.acmicpc.net/problem/11328

---
+++ KiiiimDong/[BOJ]_Strfry/[BOJ]Strfry.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+for i in range(n):
+    a, b = input().split()
+    if sorted(a) == sorted(b):
+        print('Possible')
+    else:
+        print('Impossible')
